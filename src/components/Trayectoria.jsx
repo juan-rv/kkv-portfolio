e81b3f42936fdfdf24694129ff7cba9d7d528d8f
juan-rv <juanrodriguez9502@gmail.com>
2023-05-02 19:37:18 -0500

    exit frammer

--- src/components/Trayectoria.jsx
+++ src/components/Trayectoria.jsx
@@ -8,6 +8,17 @@ const Trayectoria = () => {
     <div className="trayectoria" id="trayectoria" ref={sectionRef}>
       <div className="info_trayectoria">
         <h1> Trayectoria</h1>
+        <ul>
+          <li>bogot</li>
+          <li>Fusa</li>
+          <li>Mexico</li>
+          <li>Espa;a</li>
+          <li>Ecuador</li>
+          <li>Westcoast</li>
+          <li>Francia</li>
+          <li>Liverpool</li>
+          <li>Manchester</li>
+        </ul>
       </div>
       <div className="info_dos_trayectoria">
         <h2>Aqui lo que van son las imagenes</h2>
