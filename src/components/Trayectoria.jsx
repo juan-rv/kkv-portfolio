93b3c903397e8e5393d932736009e5f93cbb0246
juan-rv <juanrodriguez9502@gmail.com>
2023-05-03 21:19:14 -0500

    second carrousel

--- src/components/Trayectoria.jsx
+++ src/components/Trayectoria.jsx
@@ -1,9 +1,45 @@
 import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import uno from "../assets/flyers/1.jpg";
+import dos from "../assets/flyers/2.jpg";
+import tres from "../assets/flyers/3.jpg";
+import cuatro from "../assets/flyers/4.jpeg";
+import cinco from "../assets/flyers/5.jpeg";
+import seis from "../assets/flyers/6.jpeg";
+import siete from "../assets/flyers/7.jpeg";
+import ocho from "../assets/flyers/8.jpeg";
+import nueve from "../assets/flyers/9.jpeg";
+import diez from "../assets/flyers/10.jpeg";
+import once from "../assets/flyers/11.jpeg";
+import doce from "../assets/flyers/12.jpeg";
+import trece from "../assets/flyers/13.jpeg";
+import catorce from "../assets/flyers/14.jpeg";
+import quince from "../assets/flyers/15.jpeg";
+import diezyseis from "../assets/flyers/16.jpeg";
+import diezysiete from "../assets/flyers/17.jpeg";
+import diezyocho from "../assets/flyers/18.jpeg";
+import diezynueve from "../assets/flyers/19.jpeg";
+import veinte from "../assets/flyers/20.jpeg";
+
 import logo from "../assets/logo_big.png";
 import "../styles/Trayectoria.css";
 
 const Trayectoria = () => {
   const sectionRef = useRef();
+
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 60,
+    lazyLoad: true,
+    arrows: false,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <div className="trayectoria" id="trayectoria" ref={sectionRef}>
@@ -65,7 +101,88 @@ const Trayectoria = () => {
         <img src={logo} alt="logo kkv" />
       </div>
       <div className="info_dos_trayectoria">
-        <h2>Aqui lo que van son las imagenes</h2>
+        <Slider {...settings}>
+          <div>
+            {" "}
+            <img src={uno} alt="uno" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={dos} alt="dos" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={tres} alt="tres" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={cuatro} alt="cuatro" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={cinco} alt="cinco" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={seis} alt="seis" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={siete} alt="siete" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={ocho} alt="ocho" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={nueve} alt="nueve" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={diez} alt="diez" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={once} alt="once" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={doce} alt="doce" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={trece} alt="trece" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={catorce} alt="catorce" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={quince} alt="quince" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={diezyseis} alt="diezyseis" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={diezysiete} alt="diezysiete" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={diezyocho} alt="diezyocho" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={diezynueve} alt="diezynueve" />{" "}
+          </div>
+          <div>
+            {" "}
+            <img src={veinte} alt="veinte" />{" "}
+          </div>
+        </Slider>
       </div>
     </div>
   );
