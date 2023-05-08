975b74f655a4759cb1481bd076bc459b753f37bb
juan-rv <juanrodriguez9502@gmail.com>
2023-05-08 17:02:34 -0500

    navBar full

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useScrollInto from "../utils/hooks/useScrollInto";
 import { useTranslation } from "react-i18next";
 import {
@@ -9,13 +9,18 @@ import {
   FaTiktok,
 } from "react-icons/fa";
 import logo from "../assets/logoPrincipalBlanco.png";
+import espanol from "../assets/colombia.png";
+import ingles from "../assets/jamaica.png";
 import "../styles/Nav.css";
 
 const HeaderNav = () => {
+  const [active, setActive] = useState("home");
+
   const scrollTo = useScrollInto();
 
   const handleLink = (section) => {
     scrollTo(section);
+    setActive(section);
   };
 
   const [t, i18n] = useTranslation("global");
@@ -37,23 +42,40 @@ const HeaderNav = () => {
   }
 
   return (
-    <header className="header">
-      <img src={logo} alt="logo kkv" />
+    <div className="header">
+      <img className="logo" src={logo} alt="logo kkv" />
+
       <nav className="nav">
         <ul>
-          <a title="intro" href="#home" onClick={() => handleLink("intro")}>
+          <a
+            title="intro"
+            href="#home"
+            onClick={() => handleLink("intro")}
+            className={active === "intro" ? "active" : ""}
+          >
             {t("nav.home")}
           </a>
-          <a title="bio" href="#bio" onClick={() => handleLink("bio")}>
+          <a
+            title="bio"
+            href="#bio"
+            onClick={() => handleLink("bio")}
+            className={active === "bio" ? "active" : ""}
+          >
             {t("nav.bio")}
           </a>
-          <a title="band" href="#band" onClick={() => handleLink("band")}>
+          <a
+            title="band"
+            href="#band"
+            onClick={() => handleLink("band")}
+            className={active === "band" ? "active" : ""}
+          >
             {t("nav.band")}
           </a>
           <a
             title="trayectoria"
             href="#trayectoria"
             onClick={() => handleLink("trayectoria")}
+            className={active === "trayectoria" ? "active" : ""}
           >
             {t("nav.trajectory")}
           </a>
@@ -84,12 +106,21 @@ const HeaderNav = () => {
           </div>
         </ul>
       </nav>
-
       <div className="buttons_lang">
-        <button onClick={() => changeLanguage("es")}>es</button>
-        <button onClick={() => changeLanguage("en")}>en</button>
+        <buttom
+          className={`eng ${isSelected("es")}`}
+          onClick={() => changeLanguage("es")}
+        >
+          <img className="flag" src={espanol} alt="espanol" />
+        </buttom>
+        <buttom
+          className={`eng ${isSelected("en")}`}
+          onClick={() => changeLanguage("en")}
+        >
+          <img className="flag" src={ingles} alt="ingles" />
+        </buttom>
       </div>
-    </header>
+    </div>
   );
 };
 export default HeaderNav;
