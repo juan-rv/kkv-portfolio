d26b8fd4565b7d8913f5662c0bffe8ecfaf431f6
juan-rv <juanrodriguez9502@gmail.com>
2023-05-08 15:49:58 -0500

    estilos navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect } from "react";
 import useScrollInto from "../utils/hooks/useScrollInto";
 import { useTranslation } from "react-i18next";
+import {
+  FaSpotify,
+  FaYoutube,
+  FaInstagram,
+  FaFacebook,
+  FaTiktok,
+} from "react-icons/fa";
+import logo from "../assets/logoPrincipalBlanco.png";
 import "../styles/Nav.css";
 
 const HeaderNav = () => {
@@ -30,36 +38,53 @@ const HeaderNav = () => {
 
   return (
     <header className="header">
-      <div>
-        <nav className="nav">
-          <ul>
-            <a title="intro" href="#home" onClick={() => handleLink("intro")}>
-              {t("nav.home")}
-            </a>
-            <a title="bio" href="#bio" onClick={() => handleLink("bio")}>
-              {t("nav.bio")}
-            </a>
-            <a title="band" href="#band" onClick={() => handleLink("band")}>
-              {t("nav.band")}
-            </a>
-            <a
-              title="trayectoria"
-              href="#trayectoria"
-              onClick={() => handleLink("trayectoria")}
-            >
-              {t("nav.trajectory")}
-            </a>
+      <img src={logo} alt="logo kkv" />
+      <nav className="nav">
+        <ul>
+          <a title="intro" href="#home" onClick={() => handleLink("intro")}>
+            {t("nav.home")}
+          </a>
+          <a title="bio" href="#bio" onClick={() => handleLink("bio")}>
+            {t("nav.bio")}
+          </a>
+          <a title="band" href="#band" onClick={() => handleLink("band")}>
+            {t("nav.band")}
+          </a>
+          <a
+            title="trayectoria"
+            href="#trayectoria"
+            onClick={() => handleLink("trayectoria")}
+          >
+            {t("nav.trajectory")}
+          </a>
 
-            <a
-              title="plataformas"
-              href="#plataformas"
-              onClick={() => handleLink("plataformas")}
-            >
-              {t("nav.platforms")}
+          <a
+            title="plataformas"
+            href="#plataformas"
+            onClick={() => handleLink("plataformas")}
+          >
+            {t("nav.platforms")}
+          </a>
+          <div className="icons">
+            <a href="">
+              <FaSpotify />
             </a>
-          </ul>
-        </nav>
-      </div>
+            <a href="">
+              <FaYoutube />
+            </a>
+            <a href="">
+              <FaInstagram />
+            </a>
+            <a href="">
+              <FaFacebook />
+            </a>
+            <a href="">
+              <FaTiktok />
+            </a>
+          </div>
+        </ul>
+      </nav>
+
       <div className="buttons_lang">
         <button onClick={() => changeLanguage("es")}>es</button>
         <button onClick={() => changeLanguage("en")}>en</button>
