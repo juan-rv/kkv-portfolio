88f5e6ccae3cc89428955d01a797890d1b8d8547
juan-rv <juanrodriguez9502@gmail.com>
2023-05-11 23:42:07 -0500

    prueba con plyr

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import "../styles/Music.css";
+import React, { useEffect, useRef } from "react";
+import Plyr from "plyr";
+import "plyr/dist/plyr.css";
+import "../styles/Music.css"; // Importa tu archivo de estilos CSS personalizado aquí
+import song from "../assets/audio/dano.mp3";
 
 const Music = () => {
+  const audioRef = useRef();
+
+  useEffect(() => {
+    // Inicializar el reproductor Plyr cuando el componente se monta
+    const player = new Plyr(audioRef.current, { autoplay: true });
+
+    // Opcional: Suscribirse a eventos de Plyr según tus necesidades
+    player.on("play", () => {
+      console.log("El audio ha comenzado a reproducirse");
+    });
+
+    // Opcional: Limpieza al desmontar el componente
+    return () => {
+      player.destroy();
+    };
+  }, []);
+
   return (
-    <div className="music-player">
-      <audio controls>
-        <source
-          src="https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
-          type="audio/mp3"
-        />
-        Tu navegador no soporta audio HTML5.
+    <div className="audio-player-container">
+      {/* Agrega la clase para el contenedor */}
+      <audio ref={audioRef}>
+        <source src={song} type="audio/mp3" />
       </audio>
-      <div className="button-container">
-        <button className="play-button">Play</button>
-        <button className="pause-button">Pause</button>
-        <button className="next-button">Next</button>
-      </div>
     </div>
   );
 };
