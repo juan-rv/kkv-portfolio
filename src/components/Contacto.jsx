8f5ebfc80b41b3add5d86468dcd3c4a7aa2fe020
juan-rv <juanrodriguez9502@gmail.com>
2023-05-18 21:46:54 -0500

    agregando el form

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -6,6 +6,21 @@ const Contacto = () => {
 return (
     <div className='contacto' id='contacto' ref={sectionRef}>
         <h1>contacto acontinuacion:</h1>
+        <form >
+    <div>
+        <label htmlFor="name">Nombre:</label>
+        <input/>
+    </div>
+    <div>
+        <label htmlFor="email">Correo electrónico:</label>
+        <input/>
+    </div>
+    <div>
+        <label htmlFor="message">Mensaje:</label>
+        <textarea/>
+    </div>
+    <button type="submit">Enviar</button>
+    </form>
     </div>
 )
 }
