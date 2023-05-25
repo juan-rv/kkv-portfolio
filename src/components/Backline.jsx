011b0c2c6c1ad5494901a867165301754ade5fee
juan-rv <juanrodriguez9502@gmail.com>
2023-05-24 22:15:51 -0500

    pruebas commit

--- src/components/Backline.jsx
+++ src/components/Backline.jsx
@@ -1,16 +1,19 @@
 import React, { useRef } from "react";
-import "../styles/BackLine.css"
+import "../styles/BackLine.css";
 
 const Backline = () => {
   const sectionRef = useRef();
 
-
   return (
-    <div className="backline" id="backLine" ref ={sectionRef}>
+    <div className="backline" id="backLine" ref={sectionRef}>
       <h1>
         En esta seccion va el backline y los requerimientos minimos que se
         necesitasn para poder tocar
       </h1>
+
+      <h2>
+        Una lista con los requerimientos basicos de la agrupacion musical kkv
+      </h2>
     </div>
   );
 };
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,45 +1,47 @@
-import React, { useRef } from 'react'
-import "../styles/Contacto.css"
+import React, { useRef } from "react";
+import "../styles/Contacto.css";
 
 const Contacto = () => {
-    const sectionRef = useRef()
+  const sectionRef = useRef();
 
-return (
-<div className="contact-form-container">
-    <h2 className="contact-form-title">Reserva</h2>
-    <form>
+  return (
+    <div className="contact-form-container">
+      <h2 className="contact-form-title">Reserva</h2>
+      <form>
         <div className="form-group">
-        <label htmlFor="name">Nombre:</label>
-        <input/>
-            </div>
-            <div className="form-group">
-        <label htmlFor="name">Locacion:</label>
-        <input/>
+          <label htmlFor="name">Nombre:</label>
+          <input />
         </div>
         <div className="form-group">
-        <label htmlFor="name">Email:</label>
-        <input/>
+          <label htmlFor="name">Locacion:</label>
+          <input />
         </div>
         <div className="form-group">
-                <label htmlFor="email">Whatsapp:</label>
-        <input/>
+          <label htmlFor="name">Email:</label>
+          <input />
         </div>
         <div className="form-group">
-                <label htmlFor="email">Tipo de evento:</label>
-        <input/>
+          <label htmlFor="email">Whatsapp:</label>
+          <input />
         </div>
         <div className="form-group">
-                <label htmlFor="email">Nombre del Evento:</label>
-        <input/>
+          <label htmlFor="email">Tipo de evento:</label>
+          <input />
         </div>
         <div className="form-group">
-        <label htmlFor="message">Mensaje:</label>
-        <textarea/>
+          <label htmlFor="email">Nombre del Evento:</label>
+          <input />
         </div>
-        <button type="submit" className="submit-button">Enviar</button>
-    </form>
+        <div className="form-group">
+          <label htmlFor="message">Mensaje:</label>
+          <textarea />
+        </div>
+        <button type="submit" className="submit-button">
+          Enviar
+        </button>
+      </form>
     </div>
-)
-}
+  );
+};
 
-export default Contacto
+export default Contacto;
